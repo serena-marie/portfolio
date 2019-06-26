6c790477b794eb98b3facd867708f220760907b5
serena-marie <serena.gutierrez0@gmail.com>
2019-06-26 15:21:50 -0700

    Adding routes

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div class="centeredHeader">
+                Hi! My name is Serena. I am currently working on this.
+            </div>
+        ); //end return
+    }//end render
+}//end class
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 import * as serviceWorker from './serviceWorker';
+import routes from './routes.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
