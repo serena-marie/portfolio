d2b63ccc5ff748fbbc23758a2efdbd2ab3d61156
serena-marie <serena.gutierrez0@gmail.com>
2019-06-30 22:36:00 -0500

    test..wip

--- src/App.js
+++ src/App.js
@@ -6,27 +6,17 @@ import Header from './components/Header.js';
 import Home from './components/Home.js';
 import Projects from './components/Projects.js';
 
-function initReactGA(){
+ReactGA.initialize('UA-143101404-1');
+/*function initReactGA(){
   ReactGA.initialize('UA-143101404-1');
   //ReactGA.pageview('#'); //test
-}
+}*/
 function App() {
   return (
-    <head></head>
     <HashRouter>
       <div class="wrapper">
         {/* Header */}
         <div class="appHeader">
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143101404-1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)};
-          gtag('js', new Date());
-
-          gtag('config', 'UA-143101404-1');
-          </script>
-
-
           <Header/>
         </div>
 
