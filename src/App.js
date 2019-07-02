6b2757bb9913ff6d9ce63d71a64725eff7430f8b
serena-marie <serena.gutierrez0@gmail.com>
2019-07-02 18:18:48 -0500

    Analytics working

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import {Route, HashRouter} from "react-router-dom";
 import ReactGA from 'react-ga';
+import {Route, HashRouter} from "react-router-dom";
 import './App.css';
 import Header from './components/Header.js';
 import Home from './components/Home.js';
 import Projects from './components/Projects.js';
 
-ReactGA.initialize('UA-143101404-1');
-function initReactGA(){
-  //ReactGA.initialize('UA-143101404-1');
-  ReactGA.pageview('window.location.hash'); //test
-}
-function App() {
-  return (
-    <HashRouter onUpdate={initReactGA}>
-      <div class="wrapper">
-        {/* Header */}
-        <div class="appHeader">
-          <Header/>
-        </div>
+class App extends React.Component {
 
-        {/* Body  */}
-        <div class="appBody"> 
-          <Route exact path="/" component={Home}/>
-          <Route path="/projects" component={Projects}/>
-        </div>
-  
-        {/* Footer */}
-        <div class="appFooter"> </div>
-      </div> 
-    </HashRouter>
+  constructor(props){
+    super(props);
     
-  );
+    ReactGA.initialize('UA-143101404-1');
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  render(){
+    return (
+      <HashRouter>
+        <div class="wrapper">
+          {/* Header */}
+          <div class="appHeader">
+            <Header/>
+          </div>
+
+          {/* Body */}
+          <div class="appBody"> 
+            <Route exact path="/" component={Home}/>
+            <Route path="/projects" component={Projects}/>
+          </div>
+    
+          {/* Footer */}
+          <div class="appFooter"> </div>
+        </div> 
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
