b8e4d2416fca6aff1528f8c56797c5c406a5c340
serena-marie <serena.gutierrez0@gmail.com>
2019-07-02 19:16:21 -0500

    Adding projects json and placeholder for recent work

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import '../App.css';
+import data from '../data/projects.json';
 
-class Project extends React.Component{
-    render(){
-        return(
-           <div class="wrapper">
-               ** Work In Progress ** 
-           </div>
-        ); //end return
-    }//end render
-}//end class
+class Project extends React.Component {
+  render (){
+    return(
+      <div>
+        <h1>Recent Work:</h1>
+        {data.map((detail, index)=>{
+          return <div>
+            <table>
+              <tr>
+                <td>
+                  <h2>{detail.title}</h2>
+                  <p>{detail.info}</p>
+                </td>
+                <td>
+                  <img src={require('../assets/'+detail.image)} width="150px" height="150px"/>
+                </td>
+              </tr>
+              <tr>
+                <td>{detail.tech}</td>
+                <td>{detail.url}</td>
+              </tr>
+            
+            </table>
+            </div>
+        })}
+      </div> 
+    )
+  }
+}
 
 export default Project;
