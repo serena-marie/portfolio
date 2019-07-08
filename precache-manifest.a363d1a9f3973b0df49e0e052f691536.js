d5426cf8bda476ebc68bad658cd952edf792445b
serena-marie <serena.gutierrez0@gmail.com>
2019-07-07 22:49:35 -0500

    Updates

---
+++ precache-manifest.a363d1a9f3973b0df49e0e052f691536.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd362c2febbed9356bd0add464d09991",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9958f396764cfdbff4e0",
+    "url": "/portfolio/static/css/main.1e1a5205.chunk.css"
+  },
+  {
+    "revision": "8214df675f4775669e78",
+    "url": "/portfolio/static/js/2.bf8f35ea.chunk.js"
+  },
+  {
+    "revision": "9958f396764cfdbff4e0",
+    "url": "/portfolio/static/js/main.4451d9b6.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ecd62da3dee774b29f5df5f5e4313607",
+    "url": "/portfolio/static/media/color.ecd62da3.png"
+  },
+  {
+    "revision": "5421734ba086f44353e9f6eb73a8c04d",
+    "url": "/portfolio/static/media/emo.5421734b.png"
+  },
+  {
+    "revision": "274e7e0d250f56ac02cbebb78d10ba05",
+    "url": "/portfolio/static/media/me.274e7e0d.jpg"
+  },
+  {
+    "revision": "ed224df86d3f055749093744113c9d4f",
+    "url": "/portfolio/static/media/ootw.ed224df8.png"
+  },
+  {
+    "revision": "2b1e8c0ea40bad4b013e88092d008ff2",
+    "url": "/portfolio/static/media/test.2b1e8c0e.jpg"
+  },
+  {
+    "revision": "2c394411c8469cdecd50ec724c811e12",
+    "url": "/portfolio/static/media/todo.2c394411.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f5ff7ecec25787e17a376fce157c26c8.js"
+  "/portfolio/precache-manifest.a363d1a9f3973b0df49e0e052f691536.js"
 );
 
 self.addEventListener('message', (event) => {
