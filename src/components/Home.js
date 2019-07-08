a57e164cd1b21550fd2f79cc113d89c33bcd75b3
serena-marie <serena.gutierrez0@gmail.com>
2019-07-08 12:47:21 -0500

    Updating min height

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,16 +6,17 @@ class Home extends React.Component{
     render(){
         return(
             <div>
-            <div className="rowC">
-                <div className="homeImageDiv">
-                    <img src={me} className="homeImage" alt="it me"/>
+                <div className="rowC">
+                    <div className="homeImageDiv">
+                        <img src={me} className="homeImage" alt="it me"/>
+                    </div>
+                    <div className="centeredHeader">
+                        Hi! My name is Serena. I'm a San Francisco based Software Engineer by way of Austin, Texas. 
+                    </div>
                 </div>
-                <div className="centeredHeader">
-                    Hi! My name is Serena. I'm a San Francisco based Software Engineer by way of Austin, Texas. 
-                </div>
-            </div>
             <p>I'm a recent Computer Science graduate from Texas State University. I love learning new technologies and am constantly striving to challenge myself. I thrive in agile environments because I believe communication is of the utmost importance.</p>
-            <p style={{fontStyle: 'italic',}}>This website is a work in progress!</p>
+
+            <p style={{fontStyle: 'italic'}}>This website is a work in progress!</p>
             </div>
             
         ); //end return
