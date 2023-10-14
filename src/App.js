6773c5cf7843de402cffa4d1cca3d30d97c575a1
Serena <serena.gutierrez0@gmail.com>
2023-10-14 12:29:09 -0700

    refactor: Modernize styling and remove unused components for redesign - Remove Header and Footer components - Remove associated Header & Footer styles - Update App styles to scss - Add variables for consistent theming

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import { Route, HashRouter } from 'react-router-dom'
-import './styles/App.css'
-import Header from './components/Header.js'
+import './styles/App.scss'
+import './styles/reset.css'
 import Home from './pages/Home'
-import Projects from './pages/Projects.js'
-import Footer from './components/Footer.js'
+import Projects from './pages/Projects'
 
 class App extends React.Component {
   render () {
     return (
       <HashRouter>
         <div className="wrapper">
-          <div className="appHeader">
-            <Header/>
-          </div>
-
           <div className="appBody">
             <Route exact path="/" component={Home}/>
             <Route path="/projects" component={Projects}/>
-          </div>
-
-          <div className="appFooter">
-            <Footer/>
           </div>
         </div>
       </HashRouter>
