f2bd17fbef6b433e0c76533edc7cf48a07f23ebb
Serena <serena.gutierrez0@gmail.com>
2023-10-28 14:33:44 -0700

    feat: Add media queries for responsiveness

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,10 +14,10 @@ export default function About() {
         <div className="title">About Serena</div>
         <div className="bio">
           <p>
-            Howdy! I'm a software engineer based in San Francisco by way of Austin, Texas. I primarily work with all things JavaScript -- Vue, Node, React, etc! With that said, I never say no to a challenge, enjoy learning the ins and outs of programming languages, and have fun working with all parts of the stack.
+            Howdy! I'm a software engineer based in San Francisco by way of Austin, Texas. I primarily work with all things JavaScript -- Vue, Node, React, etc. With that said, I never say no to a challenge, enjoy learning the ins and outs of programming languages, and have fun working with all parts of the stack.
           </p>
           <p>
-            I previously worked as a team of one, developing and maintaining features for an internal web-based tool used by the Support and Engineering teams at Lightstream. I also have a professional background in Customer Support, and I constantly think about the user experience. In 2019, I graduated from Texas State University with a Bachelor's in Computer Science. Throughout college, I worked with C/C++ and Python and some of my favorite electives were Maching Learning, Computer Vision, and Statiscal Genetics + Bioinformatics.
+            I previously developed and maintained features for an internal web-based tool used by the Support and Engineering teams at Lightstream. I also have a background in Customer Support, and I constantly think about the user experience. In 2019, I graduated from Texas State University with a Bachelor's in Computer Science. Throughout college, I worked with C/C++ and Python and some of my favorite electives were Maching Learning, Computer Vision, Statistical Genetics + Bioinformatics and a project course where I got to work closely with other CS students developing a mobile app in an agile environment.
           </p>
           <p>
             When I'm not coding, you might find me drinking lots of coffee, playing video games, and nerding out about music.
