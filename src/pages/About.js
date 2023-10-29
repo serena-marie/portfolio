d330a514f8fd6742fa3edbd057cfd905693c54be
Serena <serena.gutierrez0@gmail.com>
2023-10-28 17:48:50 -0700

    fix: Revert removal of page title code, updated to use hooks

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,21 @@
+import {useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import '../styles/About.scss'
 import me from '../assets/me.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { updatePageTitle } from '../utils/updatePageTitle'
 
 export default function About() {
+  const [hasTitleUpdate, setHasTitleUpdate] = useState(false);
+
+  useEffect(() => {
+    if (!hasTitleUpdate) {
+      updatePageTitle('About');
+      setHasTitleUpdate(true);
+    }
+  }, [hasTitleUpdate]);
+
   return(
     <div className="aboutContainer">
       <div className='pictureOfMe' >
